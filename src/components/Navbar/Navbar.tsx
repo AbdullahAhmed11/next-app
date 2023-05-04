adb85159f5c227886139bfc58adc77ad26d84544
Abdullahahmed11 <abdullah77zzx@gmail.com>
2023-05-05 00:32:36 +0300

    updateTest

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,9 +3,7 @@ import React from "react";
 function Navbar() {
     return (
         <>
-            <div>
-                <h1>Hello nav</h1>
-            </div>
+            <div>test</div>
         </>
     )
 }
